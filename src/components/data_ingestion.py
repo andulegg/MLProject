f9f37f8f76718fa7aab46fb509246ea10e7e9c85
Andy Asfaw <andulegg@gmail.com>
2024-12-12 19:59:19 -0600

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -45,7 +47,7 @@ class DataIngestion:
             )
             
         except Exception as e:
-            raise CustomeException(e,sys)
+            raise CustomException(e,sys)
         
         
 if __name__ == "__main__":
@@ -53,5 +55,8 @@ if __name__ == "__main__":
     train_data,test_data =obj.initiate_data_ingestion()
     
     data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_err,test_err,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_err,test_err))
     
